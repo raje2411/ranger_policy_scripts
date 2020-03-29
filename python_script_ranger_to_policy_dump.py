a3d91855614d3922a02d868667837017e060799f
Rajesh Raman <25060965+raje2411@users.noreply.github.com>
2020-03-28 23:50:04 -0400

    Update python_script_ranger_to_policy_dump.py

--- python_script_ranger_to_policy_dump.py
+++ python_script_ranger_to_policy_dump.py
@@ -1,14 +1,40 @@
+#Python Libraries used in this program
+
 import requests
 import json
 from requests.auth import HTTPBasicAuth
-ranger_admin_url ="http://c349-node4:6080/service/public/v2/api/service/c349_hive/policy?pageSize=250000"
-username="admin"
-password="admin"
-ranger_hive_policy=requests.get(ranger_admin_url,auth=HTTPBasicAuth(username,password))
-with open("c349_policy_dump.json","w") as write_file:
-  json.dump(ranger_hive_policy.json(),write_file)
-policie_dump = ranger_hive_policy.json()
-for policy_item in policie_dump:
-     policy_name_str = str(policy_item["name"])
-     with open('%s.json' %policy_name_str,"w") as write_file:
-       json.dump(policy_item,write_file)
+
+#Example:- ranger_admin_url ="http://c349-node4:6080/service/public/v2/api/service/c349_hive/policy?pageSize=250000"
+
+#User input to collect Environment details
+
+ranger_host= str(raw_input("Ranger host name(default localhost): ") or "localhost")
+ranger_port= str(raw_input("Ranger port(default : 6080): ") or "6080")
+ssl_enabled= str(raw_input("Ranger SSL enabled ? (True or False): ") or "False") 
+if ssl_enabled == True:
+  protocol="https"
+else:
+  protocol="http"
+ranger_adm_username = str(raw_input("Ranger admin username (default : admin): ") or "admin")
+ranger_adm_passwd = str(raw_input("Ranger admin password (default : admin): ") or "admin")
+ranger_service_name = str(raw_input("Enter the exact Ranger service name to be exported(as shown in Ranger URI) :"))
+ranger_admin_url = protocol + "://"+ ranger_host + ":" + ranger_port + "/service/public/v2/api/service/" + ranger_service_name + "/policy?pageSize=250000"
+need_full_dump = str(raw_input("Do you want a full policy dump in a single file (y or n)? : ") or "n")
+
+#Pull all policies from Ranger admin for a given service
+
+ranger_policy_pull = requests.get(ranger_admin_url,auth=HTTPBasicAuth(ranger_adm_username,ranger_adm_passwd))
+
+# Full policy dump
+if need_full_dump == "y" or "Y":
+  with open("%s_policy_dump.json" %ranger_service_name,"w") as write_file:
+    json.dump(ranger_policy_pull.json(),write_file, indent=4)
+
+# Exporting each policies in to a single file with the same name as policy
+ 
+policy_dump = ranger_policy_pull.json()
+
+for policy_item in policy_dump:
+     policy_name = str(policy_item["name"])
+     with open('%s.json' %policy_name,"w") as write_file:
+       json.dump(policy_item,write_file, indent=4)
